0fd0684177a1b66d2848632b2f32373ba2dd73fe
Emmanuel SHYIRAMBERE <emashyirambere1@gmail.com>
2024-06-03 16:22:39 +0200

    Adjusted gallery layout for medium screens and added thumbnail images to the gallery items

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,13 @@ function createOverlays(item) {
   const overlays = document.createElement("div");
   overlays.classList.add("overlays");
 
+  const info = document.createElement("div");
+  info.classList.add("info");
+
   const subjectName = document.createElement("h2");
   subjectName.classList.add("subject-name");
+
+  // Splitting subject name into parts and add line break
   const subjectNameParts = item.subjectName.split(" ");
   const lastPartIndex = subjectNameParts.length - 1;
   subjectName.innerHTML =
@@ -16,8 +21,10 @@ function createOverlays(item) {
   location.classList.add("location");
   location.textContent = item.location;
 
-  overlays.appendChild(subjectName);
-  overlays.appendChild(location);
+  info.appendChild(subjectName);
+  info.appendChild(location);
+
+  overlays.appendChild(info);
 
   return overlays;
 }
@@ -28,45 +35,33 @@ function createImageContainer(item) {
   imageInteraction.classList.add("image-interaction");
 
   const overlays = createOverlays(item);
+
   const imageContainer = document.createElement("div");
   imageContainer.classList.add("image");
+
   const imageContent = document.createElement("div");
   imageContent.classList.add("imagecontent");
 
   const img = document.createElement("img");
-  img.src = item.image;
+  img.src = item.thumbNailImage || item.image;
   img.alt = item.subjectName;
-  img.addEventListener("click", () =>
-    openFullSizeImage(item.image, item.subjectName)
-  );
 
   const rectangle1 = document.createElement("div");
   rectangle1.classList.add("rectangle-1");
 
   imageContent.appendChild(img);
+
   imageContainer.appendChild(rectangle1);
   imageContainer.appendChild(imageContent);
+
   imageInteraction.appendChild(overlays);
   imageInteraction.appendChild(imageContainer);
 
+  // Swap the thumbnail with the main image after a brief delay
+  setTimeout(() => {
+    img.src = item.image;
+  }, 100);
   return imageInteraction;
-}
-
-// Function to open a full-size image view
-function openFullSizeImage(src, alt) {
-  const overlay = document.createElement("div");
-  overlay.classList.add("full-size-overlay");
-  overlay.innerHTML = `
-    <div class="full-size-container">
-      <img src="${src}" alt="${alt}" />
-      <button class="close-btn">Close</button>
-    </div>
-  `;
-  document.body.appendChild(overlay);
-
-  overlay.querySelector(".close-btn").addEventListener("click", () => {
-    document.body.removeChild(overlay);
-  });
 }
 
 // Wait for the DOM content to be fully loaded
@@ -76,25 +71,31 @@ document.addEventListener("DOMContentLoaded", function () {
       subjectName: "fennec fox",
       location: "India",
       image: "./images/fennec-fox.png",
+      thumbNailImage: "./images/fennec-fox-thumbnail.jpg",
     },
     {
       subjectName: "humpback whale",
       location: "South Africa",
       image: "./images/humpback-whale.png",
+      thumbNailImage: "./images/humpback-whale-thumbnail.jpg",
     },
     {
       subjectName: "common brown baboon",
       location: "South Africa",
       image: "./images/common-brown-baboon.png",
+      thumbNailImage: "./images/common-brown-baboon-thumbnail.jpg",
     },
     {
       subjectName: "spotted deer",
       location: "Amazon",
       image: "./images/spotted-deer.png",
+      thumbNailImage: "./images/spotted-deer-thumbnail.jpg",
     },
   ];
 
   const gallery = document.getElementById("gallery");
+
+  // Iterate over each item in the gallery data
   galleryData.forEach((item) => {
     const imageContainer = createImageContainer(item);
     gallery.appendChild(imageContainer);
